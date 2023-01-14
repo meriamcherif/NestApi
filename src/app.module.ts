22ddc990c1934547dfdfc853c0f6b1d578d0381c
letmepy <skander.soltane@pm.me>
2023-01-14 01:16:37 +0100

    add typeorm and pg connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {BookModule} from './book/book.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'ktebi',
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+    }), BookModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
